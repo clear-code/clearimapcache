a18df190a9c5672b251e7441b23120a74823dcf2
YUKI Hiroshi <shimoda@clear-code.com>
2014-01-10 12:06:34 +0900

    Activate default preferences

--- defaults/preferences/clearimapcache.js
+++ defaults/preferences/clearimapcache.js
@@ -1,7 +1,5 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
-// pref("extensions.clearimapcache.enabled", true);
-// pref("extensions.clearimapcache.clear.summary", false);
-
+pref("extensions.clearimapcache.enabled", true);
+pref("extensions.clearimapcache.clear.summary", false);
